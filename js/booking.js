bf17b00c5abb59b799dec890a6b7ab69010631bf
T. Lanette <TLPollard4516@gmail.com>
2021-06-25 20:53:44 -0400

    Updated style on form
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/booking.js
+++ js/booking.js
@@ -13,7 +13,7 @@ const time = document.getElementById('time');
 const hour = document.getElementById('hour');
 const min = document.getElementById('min');
 
-//const select = document.getElementById('am-pm');
+const select = document.getElementById('am-pm');
 const formInputArrow = document.getElementById('arrow');
 
 // Variables for Number Picker
@@ -85,7 +85,11 @@ function checkAmtPeople (people) {
 }
 
 formInputArrow.addEventListener('click', () => {
+    select.classList.toggle('transform');
 
+    if (formInputArrow.firstChild.nodeValue === 'PM') {
+    
+    }
 })
 
 iconMinus.addEventListener('click', () => {
